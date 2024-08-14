a690d67b03f50d5a1397cef0c42ae831b436ccb2
Nishant Parmar <nishant.singhparmar@ge.com>
2024-08-13 20:13:44 -0400

    defined Parameter class for more robust checking

--- tools/cli_app_class.py
+++ tools/cli_app_class.py
@@ -1,24 +1,16 @@
-from enum import StrEnum, unique
-from typing import Annotated, Optional, Self
+from copy import deepcopy
+from typing import Annotated, Optional, Self, Type
 from rich import print
 from rich.console import Console
 from rich.panel import Panel
 from rich.pretty import Pretty
-from pydantic import BaseModel, ConfigDict, model_validator
+from pydantic import BaseModel, ConfigDict, ValidationError, model_validator
 import typer
-from utils import reverse_lookup
+from tools.utils import parse_integer_inputs, parse_string_inputs, reverse_lookup, Parameter, describe_parameters
 
 app = typer.Typer(no_args_is_help=True)
 console = Console()
-null_params = list()
-
-
-@unique
-class AppState(StrEnum):
-    STOPPED = "stopped"
-    RUNNING = "running"
-    EXITED = "exited"
-    TERMINATED = "terminated"
+user_params: dict = dict()
 
 
 class ParametersAbsentException(Exception):
@@ -27,99 +19,139 @@ class ParametersAbsentException(Exception):
 
 
 class CLIParams(BaseModel):
-    strict_mode: bool
-    kafka_host: Optional[str] = None
-    kafka_port: Optional[int] = None
-    kafka_topic: Optional[str] = None
-    m_and_v_host: Optional[str] = None
-    m_and_v_port: Optional[int] = None
-    m_and_v_api_slug: Optional[str] = None
-    dispatch_file_path: Optional[str] = None
-    meas_file_path: Optional[str] = None
-    publish_delay_seconds: Optional[int] = None
-    results_delay_seconds: Optional[int] = None
-    results_file_path: Optional[str] = None
+    strict_mode: Parameter
+    dispatch_file_path: Optional[Parameter] = None
+    meas_file_path: Optional[Parameter] = None
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
 
     @model_validator(mode="after")
     def validate_params(self) -> Self:
-        global null_params
-        params = self.model_dump()
+        global user_params
+        params = self.model_dump(mode="python")
         params.pop("strict_mode")
-        null_params = reverse_lookup(params).get(None)
-
-        if not all(params.values()):
+        # user_params = reverse_lookup(params)
+        user_params = params
+        if None in reverse_lookup(user_params):
             raise ParametersAbsentException("These user parameters were not present:")
         return self
 
 
-def prompt_for_params(param_list: list) -> dict:
-    filled_params = dict()
+class PublishParams(CLIParams):
+    kafka_host: Optional[Parameter] = None
+    kafka_port: Optional[Parameter] = None
+    kafka_topic: Optional[Parameter] = None
+    m_and_v_host: Optional[Parameter] = None
+    m_and_v_port: Optional[Parameter] = None
+    m_and_v_api_slug: Optional[Parameter] = None
+    publish_delay_seconds: Optional[Parameter] = None
+    results_delay_seconds: Optional[Parameter] = None
+    results_file_path: Optional[Parameter] = None
+
+
+class VerifyParams(CLIParams):
+    ...
+
+
+def prompt_for_params(parameters: dict[str, Parameter]) -> dict:
+    filled_params = deepcopy(parameters)
     print("Please enter values for these parameters: ")
-    while param_list:
-        param = param_list.pop(0)
-        user_input = typer.prompt(param)
-        if user_input:
-            filled_params[param] = user_input
+    for name, param in parameters.items():
+        if param.value is None:
+            filled_params[name] = Parameter(
+                name=param.name,
+                value=typer.prompt(name),
+                required=param.required
+            )
     return filled_params
 
 
-def parse_string_inputs(input_value: Optional[str] = None):
-    stripped = input_value.strip()
-    if stripped:
-        return stripped
-    return None
-
-
-def parse_integer_inputs(input_value: Optional[str] = None):
-    if input_value:
-        return max(0, int(input_value))
-    return None
-
-
-@app.command("publish")
-def main(
-        strict_mode: Annotated[bool, typer.Option()] = False,
-        kafka_host: Annotated[Optional[str], typer.Argument(parser=parse_string_inputs)] = None,
-        kafka_port: Annotated[Optional[int], typer.Argument(parser=parse_integer_inputs)] = None,
-        kafka_topic: Annotated[Optional[str], typer.Argument(parser=parse_string_inputs)] = None,
-        m_and_v_host: Annotated[Optional[str], typer.Argument(parser=parse_string_inputs)] = None,
-        m_and_v_port: Annotated[Optional[int], typer.Argument()] = None,
-        m_and_v_api_slug: Annotated[Optional[str], typer.Argument(parser=parse_string_inputs)] = None,
-        dispatch_file_path: Annotated[Optional[str], typer.Argument(parser=parse_string_inputs)] = None,
-        meas_file_path: Annotated[Optional[str], typer.Argument(parser=parse_string_inputs)] = None,
-        publish_delay_seconds: Annotated[Optional[int], typer.Argument()] = None,
-        results_delay_seconds: Annotated[Optional[int], typer.Argument()] = None,
-        results_file_path: Annotated[Optional[str], typer.Argument(parser=parse_string_inputs)] = None,
-        suppress_traceback: Annotated[bool, typer.Option()] = False,
+def handle_incomplete_parameters(
+        app_mode: Type[PublishParams] | Type[VerifyParams],
+        strict_mode: bool,
+        suppress_traceback: bool,
+        exception: ParametersAbsentException
 ):
-    global null_params
+    preamble = f"{exception}\n"
+    if not suppress_traceback:
+        console.print_exception(show_locals=False)
+        preamble = ""
+    null_params = reverse_lookup(user_params).get(None)
+    print(Panel.fit(f"{preamble}{null_params}", border_style="red"))
     cli_params = None
     try:
-        cli_params = CLIParams(
-            strict_mode=strict_mode,
-            kafka_host=kafka_host,
-            kafka_port=kafka_port,
-            kafka_topic=kafka_topic,
-            m_and_v_host=m_and_v_host,
-            m_and_v_port=m_and_v_port,
-            m_and_v_api_slug=m_and_v_api_slug,
-            dispatch_file_path=dispatch_file_path,
-            meas_file_path=meas_file_path,
-            publish_delay_seconds=publish_delay_seconds,
-            results_delay_seconds=results_delay_seconds,
-            results_file_path=results_file_path,
+        cli_params = app_mode(
+            strict_mode=Parameter(name="strict_mode", value=strict_mode),
+            **prompt_for_params(user_params)
+        )
+    except ValidationError as ve:
+        print(Panel.fit(ve))
+    return cli_params
+
+
+@app.command()
+def publish(
+        strict_mode: Annotated[bool, typer.Option()] = False,
+        kafka_host: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        kafka_port: Annotated[Optional[int], typer.Option(parser=parse_integer_inputs)] = None,
+        kafka_topic: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        m_and_v_host: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        m_and_v_port: Annotated[Optional[int], typer.Option(parser=parse_integer_inputs)] = None,
+        m_and_v_api_slug: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        dispatch_file_path: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        meas_file_path: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        publish_delay_seconds: Annotated[Optional[int], typer.Option(parser=parse_integer_inputs)] = None,
+        results_delay_seconds: Annotated[Optional[int], typer.Option(parser=parse_integer_inputs)] = None,
+        results_file_path: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        suppress_traceback: Annotated[bool, typer.Option()] = True,
+):
+    global user_params
+    cli_params = None
+    try:
+        cli_params = PublishParams(
+            strict_mode=Parameter(name="strict_mode", value=strict_mode, required=False),
+            kafka_host=Parameter(name="kafka_host", value=kafka_host),
+            kafka_port=Parameter(name="kafka_port", value=kafka_port),
+            kafka_topic=Parameter(name="kafka_topic", value=kafka_topic),
+            m_and_v_host=Parameter(name="m_and_v_host", value=m_and_v_host),
+            m_and_v_port=Parameter(name="m_and_v_port", value=m_and_v_port),
+            m_and_v_api_slug=Parameter(name="m_and_v_api_slug", value=m_and_v_api_slug),
+            dispatch_file_path=Parameter(name="dispatch_file_path", value=dispatch_file_path),
+            meas_file_path=Parameter(name="meas_file_path", value=meas_file_path),
+            publish_delay_seconds=Parameter(name="publish_delay_seconds", value=publish_delay_seconds, required=False),
+            results_delay_seconds=Parameter(name="results_delay_seconds", value=results_delay_seconds),
+            results_file_path=Parameter(name="results_file_path", value=results_file_path, required=False),
         )
     except ParametersAbsentException as e:
-        if not suppress_traceback:
-            console.print_exception(show_locals=False)
-        print(Panel.fit(f"{e if suppress_traceback else None}\n{null_params}", border_style="red"))
-        cli_params = CLIParams(strict_mode=strict_mode, **prompt_for_params(null_params))
+        cli_params = handle_incomplete_parameters(PublishParams, strict_mode, suppress_traceback, e)
     finally:
-        pretty = Pretty(cli_params.model_dump())
-        panel = Panel(pretty)
-        print(panel)
+        if cli_params:
+            pretty = Pretty(describe_parameters(cli_params.model_dump()))
+            panel = Panel.fit(pretty, border_style="green")
+            print(panel)
+
+
+@app.command()
+def verify(
+        strict_mode: Annotated[bool, typer.Option()] = False,
+        dispatch_file_path: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        meas_file_path: Annotated[Optional[str], typer.Option(parser=parse_string_inputs)] = None,
+        suppress_traceback: Annotated[bool, typer.Option()] = True,
+):
+    cli_params = None
+    try:
+        cli_params = VerifyParams(
+            strict_mode=Parameter(name="strict_mode", value=strict_mode),
+            dispatch_file_path=Parameter(name="dispatch_file_path", value=dispatch_file_path),
+            meas_file_path=Parameter(name="meas_file_path", value=meas_file_path),
+        )
+    except ParametersAbsentException as e:
+        cli_params = handle_incomplete_parameters(VerifyParams, strict_mode, suppress_traceback, e)
+    finally:
+        if cli_params:
+            pretty = Pretty(describe_parameters(cli_params.model_dump()))
+            panel = Panel.fit(pretty, border_style="green")
+            print(panel)
 
 
 if __name__ == "__main__":
--- tools/utils.py
+++ tools/utils.py
@@ -1,8 +1,37 @@
 from collections import defaultdict
+from typing import Any, Optional
 
 
-def reverse_lookup(mapping: dict):
+class Parameter:
+    def __init__(self, name: str, value: Any, required: bool = True):
+        self.name = name
+        self.value = value
+        self.required = required
+
+
+def reverse_lookup(mapping: dict[str, Parameter]) -> defaultdict:
     reverse_mapping = defaultdict(list)
-    for key, value in mapping.items():
-        reverse_mapping[value].append(key)
+    for key, param in mapping.items():
+        if param.required:
+            reverse_mapping[param.value].append(key)
     return reverse_mapping
+
+
+def parse_string_inputs(input_value: Optional[str] = None):
+    stripped = input_value.strip()
+    if stripped:
+        return stripped
+    return None
+
+
+def parse_integer_inputs(input_value: Optional[str] = None):
+    if input_value:
+        return max(0, int(input_value))
+    return None
+
+
+def describe_parameters(user_parameters: dict[str, Parameter]):
+    description: dict[str, str] = dict()
+    for key, param in user_parameters.items():
+        description[key] = param.value
+    return description
